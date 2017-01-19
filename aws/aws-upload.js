c1a09a17a664ef35e01f6f4eaf27bf29096361df
Jesus Guerrero <rocha.jesus@hotmail.com>
2017-01-19 13:15:49 -0800

    Fixing image upload to aws s3

--- aws/aws-upload.js
+++ aws/aws-upload.js
@@ -1,5 +1,4 @@
 var aws = require('aws-sdk'),
-  request = require('request'),
   fs = require('fs');
 
 var awsS3 = new aws.S3({
@@ -9,32 +8,18 @@ var awsS3 = new aws.S3({
   region: 'us-west-1'
 });
 
-var s3Params = {
-  Bucket: 'mahrio-iot',
-  Expires: 60,
-  ACL: 'public-read'
-};
-
 module.exports = function( dataObj, callback ){
-  awsS3.Key = dataObj.filename;
+  var params = {
+    Bucket: 'mahrio-iot',
+    Key: dataObj.filename,
+    Body: fs.createReadStream(dataObj.filepath),
+    ACL: 'public-read'
+  };
 
-  awsS3.getSignedUrl('putObject', s3Params, function(err, data){
-    if( err ){ console.log('AWS SignedURL Error'); return; }
-
-    console.log('AWS SIGNED URL: ', data);
-
-    var req = request({
-      method: 'put',
-      url: data,
-      headers: { 'x-amz-acl': 'public-read', 'Content-Type': undefined }
-    }, function (err, resp, body) {
-      if (err) {
-        console.log('Error!');
-      } else {
-        callback();
-      }
-    });
-    var form = req.form();
-    form.append('file', fs.createReadStream(dataObj.filepath));
+  awsS3.putObject( params, function(err, data){
+    if( err ){
+      console.log(err);
+    }
+    callback();
   });
 };
